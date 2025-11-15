6df81a7b47f6a9954d7cf24e6973bc40263c3912
Naomi Nzioka <nziokanaomi422@gmail.com>
2025-11-15 23:50:43 +0300

    added changes to this file

--- gardenManagement.js
+++ gardenManagement.js
@@ -1,3 +1,31 @@
 const temperature = 90;
 const timeOfDay = "morning";
 let soilMoisture = 30;
+
+
+if (temperature > 80) {
+    console.log("Watering on");
+}
+else {
+    console.log("Watering on")
+}
+
+if (timeOfDay === "evening") {
+    console.log("Lights on");
+}
+
+else if (timeOfDay === "night") {
+    console.log("Lights on");
+}
+
+else {
+    console.log("Lights off");
+}
+
+
+while (soilMoisture < 40) {
+    console.log("Soilmoisture:" + soilMoisture)
+    soilMoisture = soilMoisture += 5;
+}
+console.log(soilMoisture)
+
